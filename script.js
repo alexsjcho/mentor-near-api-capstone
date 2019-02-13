d3cc767c41fdc612818338e9e0497ba588d2badd
Alex Cho <alexsjcho@gmail.com>
2019-02-13 13:12:08 -0800

    loadMentorProfilePage function demo works

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-window.jQuery ||
-  document.write(
-    '<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"></script>'
-  );
+// window.jQuery ||
+//   document.write(
+//     '<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"></script>'
+//   );
+
+watchFindMentorButton();
+
+//Watch "Find Mentor Button"
+
+function watchFindMentorButton() {
+  $("#find-mentor-start-button").click(e => {
+    e.preventDefault();
+    console.log("watchFindMentorButton works!");
+    loadMentorProfilePage();
+  });
+}
+
+//Render jQuery HTML Elements
+function loadMentorProfilePage() {
+  console.log("loadMentorProfilePage function works");
+  $("#new-page-renderer").empty();
+}
