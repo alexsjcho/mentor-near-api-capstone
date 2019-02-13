c31014bd3e986c3be010a45ec13a7049a92f080c
Alex Cho <alexsjcho@gmail.com>
2019-02-13 14:05:52 -0800

    finished watchChooseMentorButton function rendered Mentor Calendar Page successful

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-// window.jQuery ||
-//   document.write(
-//     '<script src="/docs/4.3/assets/js/vendor/jquery-slim.min.js"></script>'
-//   );
-
-watchFindMentorButton();
-
+//Call Button Functions
+$(document).ready(function() {
+  watchFindMentorButton();
+});
 //Watch "Find Mentor Button"
 
 function watchFindMentorButton() {
@@ -12,6 +9,15 @@ function watchFindMentorButton() {
     e.preventDefault();
     console.log("watchFindMentorButton works!");
     loadMentorProfilePage();
+    watchChooseMentorButton();
+  });
+}
+
+function watchChooseMentorButton() {
+  $("#new-page-render").on("click", "#choose-mentor-start-button", e => {
+    e.preventDefault();
+    console.log("watchChooseMentorButton works!");
+    loadMentorCalendarPage();
   });
 }
 
@@ -20,7 +26,7 @@ function loadMentorProfilePage() {
   console.log("loadMentorProfilePage function works");
   $("#new-page-renderer").empty();
   $("#new-page-renderer").append(
-    `<main id="new-page-renderer"  role="this is the HTML hook to render new page">
+    `
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
         <a class="navbar-brand" href="#">MentorNear</a>
 
@@ -70,9 +76,16 @@ function loadMentorProfilePage() {
                                 </a>
                             </div>
                             <div>
-                                <p>
-                                    <a class="btn btn-lg btn-primary" href="#" role="button">Choose This Mentor</a>
-                                </p>
+                            
+                                    <a
+                                    class="btn btn-primary btn-lg"
+                                    href="#"
+                                    role="load-mentor-calendar-page"
+                                    id="choose-mentor-start-button"
+                                  >
+                                    Choose This Mentor
+                                  </a>
+                            
                             </div>
                         </div>
 
@@ -104,7 +117,97 @@ function loadMentorProfilePage() {
             <p class="col-md-1"><i class="fas fa-blog"></i></p>
         </a>
     </footer>
-</main>
+    `
+  );
+}
+
+function loadMentorCalendarPage() {
+  console.log("loadMentorProfilePage function works");
+  $("#new-page-renderer").empty();
+  $("#new-page-renderer").append(
+    `
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a class="navbar-brand" href="#">MentorNear</a>
+  </nav>
+
+  <header role="banner">
+    <h1>#3 Find Time on Mentor's Calendar</h1>
+    <section role="section-instruction" class="row">
+      <div class="col-md-4 container">
+        <h3>Instructions</h3>
+        <p>
+          Select a time on your mentor's calendar
+        </p>
+      </div>
+    </section>
+  </header>
+
+  <section role="find time on mentor's calendar">
+    <div class="calendar dark">
+      <div class="calendar_header">
+        <h1 class="header_title">Mentor's Name</h1>
+        <p class="header_copy">Times I'm Free</p>
+      </div>
+      <div class="calendar_plan">
+        <div class="cl_plan">
+          <div class="cl_title">Today</div>
+          <div class="cl_copy">22nd April 2018</div>
+          <div class="cl_add">
+            <i class="fas fa-plus"></i>
+          </div>
+        </div>
+      </div>
+      <div class="calendar_events">
+        <p class="ce_title">Upcoming Events</p>
+        <div class="event_item">
+          <div class="ei_Dot dot_active"></div>
+          <div class="ei_Title">10:30 am</div>
+          <div class="ei_Copy">Monday briefing with the team</div>
+        </div>
+        <div class="event_item">
+          <div class="ei_Dot"></div>
+          <div class="ei_Title">12:00 pm</div>
+          <div class="ei_Copy">Lunch for with the besties</div>
+        </div>
+        <div class="event_item">
+          <div class="ei_Dot"></div>
+          <div class="ei_Title">13:00 pm</div>
+          <div class="ei_Copy">
+            Meet with the client for final design <br />
+            @foofinder
+          </div>
+        </div>
+        <div class="event_item">
+          <div class="ei_Dot"></div>
+          <div class="ei_Title">14:30 am</div>
+          <div class="ei_Copy">Plan event night to inspire students</div>
+        </div>
+        <div class="event_item">
+          <div class="ei_Dot"></div>
+          <div class="ei_Title">15:30 am</div>
+          <div class="ei_Copy">Add some more events to the calendar</div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <footer class="row">
+    <p class="col-md-4">&copy; Alex Cho 2019</p>
+    <p class="col-md-1">
+      <a href="https://www.linkedin.com/in/alexsjcho/"
+        ><i class="fab fa-linkedin"></i
+      ></a>
+    </p>
+    <a href="https://github.com/alexsjcho">
+      <p class="col-md-1"><i class="fab fa-github"></i></p>
+    </a>
+    <a href="https://www.mraddoil.com/">
+      <p class="col-md-1"><i class="fab fa-wordpress-simple"></i></p>
+    </a>
+    <a href="https://www.alexsjcho.com/">
+      <p class="col-md-1"><i class="fas fa-blog"></i></p>
+    </a>
+  </footer>
+
     `
   );
 }
